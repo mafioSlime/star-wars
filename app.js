e334f959f75fe569f0b9301d011aa77b72b0e55c
John Paguirigan <johnjaroldxvi@gmail.com>
2020-02-29 19:48:00 -0700

    MVP

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const characterContainer = document.querySelector(".character-container");
 const x = 1;
 const characterBtn = document.querySelector(".character-btn");
 const planetBtn = document.querySelector(".planet-btn");
-const starshipBtn = document.querySelector('.starship-btn')
+const starshipBtn = document.querySelector(".starship-btn");
 
 let randomNumber = (a, b) => {
   return Math.floor(Math.random() * a + b);
@@ -26,8 +26,11 @@ const generateCharacter = () => {
       character.mass = data.mass;
 
       characterContainer.innerHTML = `
-       <div style="border: 2px solid #ffff00;" >
+       <div>
+       <div class="char-title upsize">
       <h3>Name: ${character.name}</h3>
+      </div>
+      <div class="char-info">
       <p><span>Birthdate: ${character.birthdate}</p>
       <p>Gender: ${character.gender}</p>
       <p>Hair Color: ${character.haircolor}</p>
@@ -35,6 +38,7 @@ const generateCharacter = () => {
       <p>Eye Color: ${character.eyecolor}</p>
       <p>Height: ${character.height}</p>
       <p>Mass: ${character.mass}</p>
+      </div>
     </div>
     `;
     });
@@ -59,29 +63,32 @@ const generatePlanet = () => {
       planet.population = data.population;
 
       characterContainer.innerHTML = `
-      <div style="border: 2px solid #ffff00">
+      <div>
+      <div class="planet-title upsize">
       <h3>Name: ${planet.name}</h3>
-      <p>Rotation Period: ${planet.rotation}</p>
-      <p>Orbital Period: ${planet.orbital}</p>
+      </div>
+      <div class="planet-info">
+      <p>Rotation: ${planet.rotation}</p>
+      <p>rbital: ${planet.orbital}</p>
       <p>Diameter: ${planet.diameter}</p>
       <p>Climate: ${planet.climate}</p>
       <p>Terrain: ${planet.terrain}</p>
       <p>Surface: ${planet.surface}</p>
       <p>Population: ${planet.population}</p>
       </div>
+      </div>
       `;
     });
 };
-
 
 const generateStarship = () => {
   const url = `https://swapi.co/api/starships/${randomNumber(9, 1)}/`;
   fetch(url)
     .then(res => {
-      return res.json()
+      return res.json();
     })
     .then(data => {
-      let starship = {}
+      let starship = {};
       starship.name = data.name;
       starship.model = data.model;
       starship.manufacturer = data.manufacturer;
@@ -89,31 +96,34 @@ const generateStarship = () => {
       starship.crew = data.crew;
       starship.passengers = data.passengers;
       starship.cargo = data.cargo_capacity;
-      starship.hyperdrive = data.hyperdrive_rating
-      starship.class = data.starship_class
-
+      starship.hyperdrive = data.hyperdrive_rating;
+      starship.class = data.starship_class;
 
       characterContainer.innerHTML = `
-    <div style="border: 2px solid #ffff00">
+    <div>
+    <div class="starship-title upsize">
     <h3>Name: ${starship.name}</h3>
+    </div>
+    <div class="starship-info">
     <p>Model: ${starship.model}</p>
-    <p>Cost: ${starship.cost}</p>
+    <p>Manufacturer: ${starship.manufacturer}</p>
     <p>Crew: ${starship.crew}</p>
     <p>Passengers: ${starship.passengers}</p> 
-    <p>Hyperdrive Rating: ${starship.hyperdrive}</p>
+    <p>Hyperdrive Rating: ${starship.cost}</p>
     <p>Cargo: ${starship.cargo}</p>
     <p>Class: ${starship.class}</p>
+    <p>Hyperdrive: ${starship.hyperdrive}</p>
+    </div>
     </div>
     
     
-    `
-    })
-}
+    `;
+    });
+};
 
-
-starshipBtn.addEventListener('click', () => {
-  generateStarship()
-})
+starshipBtn.addEventListener("click", () => {
+  generateStarship();
+});
 
 characterBtn.addEventListener("click", () => {
   generateCharacter();
